b474935918a9121a6755a0f8d63fc54b51bb332c
Ferry Kirdan Agustin <33730041+ipkzone@users.noreply.github.com>
2022-07-01 22:41:40 +0000

    Create getUseragent.php
    get user agent in php

---
+++ getUseragent.php
@@ -0,0 +1,81 @@
+<?php
+function getUseragent()
+{
+    $u_agent  = $_SERVER['HTTP_USER_AGENT'];
+    $bname    = 'Unknown';
+    $platform = 'Unknown';
+    $version  = "";
+    //First get the platform?
+    if (preg_match('/linux/i', $u_agent)) {
+        $platform = 'linux';
+    } elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
+        $platform = 'mac';
+    } elseif (preg_match('/windows|win32/i', $u_agent)) {
+        $platform = 'windows';
+    }
+    
+    if (preg_match('/MSIE/i', $u_agent) && !preg_match('/Opera/i', $u_agent)) {
+        $bname = 'Internet Explorer';
+        $ub    = "MSIE";
+    } elseif (preg_match('/Firefox/i', $u_agent)) {
+        $bname = 'Mozilla Firefox';
+        $ub    = "Firefox";
+    } elseif (preg_match('/Chrome/i', $u_agent)) {
+        $bname = 'Google Chrome';
+        $ub    = "Chrome";
+    } elseif (preg_match('/Safari/i', $u_agent)) {
+        $bname = 'Apple Safari';
+        $ub    = "Safari";
+    } elseif (preg_match('/Opera/i', $u_agent)) {
+        $bname = 'Opera';
+        $ub    = "Opera";
+    } elseif (preg_match('/Netscape/i', $u_agent)) {
+        $bname = 'Netscape';
+        $ub    = "Netscape";
+    } else {
+        $bname = 'Internet Explorer';
+        $ub    = "MSIE";
+    }
+    
+    // To get correct version number
+    $known   = array(
+        'Version',
+        $ub,
+        'other'
+    );
+    $pattern = '#(?<browser>' . join('|', $known) . ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+    if (!preg_match_all($pattern, $u_agent, $matches)) {
+        // we have no matching number just continue
+    }
+    
+    //TO find out useragent version
+    $i = count($matches['browser']);
+    if ($i == 0) {
+        $version = 11;
+    } elseif ($i != 1 && $i != 0) {
+        if (strripos($u_agent, "Version") < strripos($u_agent, $ub)) {
+            $version = $matches['version'][0];
+        } else {
+            $version = $matches['version'][1];
+        }
+    } else {
+        $version = $matches['version'][0];
+    }
+    if ($version == null || $version == "") {
+        $version = "?";
+    }
+    
+    return array(
+        'userAgent' => $u_agent,
+        'name' => $bname,
+        'version' => $version,
+        'platform' => $platform,
+        'pattern' => $pattern
+    );
+}
+ 
+ 
+echo '<pre>';
+print_r(getUseragent());   //Print result
+ 
+?>
